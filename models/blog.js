b5f81096c2035468f20fb0a6ecafae2279ebe90f
REXtheRIPPERSaadh <mrsaadh39@gmail.com>
2024-10-27 22:28:30 +0530

    Update blog.js

--- models/blog.js
+++ models/blog.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+// Blog schema
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String, // URL for the image
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
 
